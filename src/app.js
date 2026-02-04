d794854c0a8636bbc76d2bc0588942a868f6b0bb
Kartikay1745 <kartikay.sinha@revolvespl.com>
2026-02-04 10:17:54 -0500

    multipled-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+// app.use("/route", rH, [rH2, rH3], rH4, rH5);
 
-// This will only handle GET call to /user
-app.get("/user/:userId/:name/:password", (req,res) => {
-    console.log(req.params)
-    res.send({firstName: "Kartikay", lastName: "Sinha"});
-});
+app.use(
+  "/user",
+  [(req, res, next) => {
+    console.log("Handling the route user");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2");
+    // res.send("2nd Response!!");
+    next();
+  },
+   (req, res, next) => {
+    console.log("Handling the route user 3");
+    // res.send("3rd Response!!");
+    next();
+  },
+   (req, res, next) => {
+    console.log("Handling the route user 4");
+    // res.send("4th Response!!");
+    next();
+  },]
 
+);
 
 app.listen(7777, () => {
-    console.log("Server is successfully listening on port 7777");
+  console.log("Server is successfully listening on port 7777");
 });
-
-
