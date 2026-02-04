4b2c2aa9b3e9e0a9bd3c1a1d3d073422d8d69bd4
Kartikay1745 <kartikay.sinha@revolvespl.com>
2026-02-04 08:38:28 -0500

    exploring HTTP method

--- src/app.js
+++ src/app.js
@@ -2,17 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req,res) => {
-    res.send("Hello Hello Hello");
-})
+app.use("/user", (req, res) => {
+    res.send("HAHAHAHAHA");
+});
 
+// This will only handle GET call to /user
+app.get("/user", (req,res) => {
+    res.send({firstName: "Kartikay", lastName: "Sinha"});
+});
+
+app.post("/user", (req, res) => {
+    // console.log("Save data to the database");
+    //Saving data to DB
+    res.send("Data Successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully");
+});
+
+
+
+
+
+// this will match all the HTTP method API calls to /test
 app.use("/test", (req,res) => {
     res.send("Hello from the server");
 })
 
-app.use("/", (req,res) => {
-    res.send("Namaste from the dashboard");
-})
+
+
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777");
