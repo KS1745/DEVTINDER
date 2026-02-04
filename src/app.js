90f13786f98023f19abebd87a597b4fdebfdb5c1
Kartikay1745 <kartikay.sinha@revolvespl.com>
2026-02-04 09:15:59 -0500

    Explored Various Routes

--- src/app.js
+++ src/app.js
@@ -2,35 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use("/user", (req, res) => {
-    res.send("HAHAHAHAHA");
-});
 
 // This will only handle GET call to /user
-app.get("/user", (req,res) => {
+app.get("/user/:userId/:name/:password", (req,res) => {
+    console.log(req.params)
     res.send({firstName: "Kartikay", lastName: "Sinha"});
 });
-
-app.post("/user", (req, res) => {
-    // console.log("Save data to the database");
-    //Saving data to DB
-    res.send("Data Successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("User deleted successfully");
-});
-
-
-
-
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req,res) => {
-    res.send("Hello from the server");
-})
-
-
 
 
 app.listen(7777, () => {
